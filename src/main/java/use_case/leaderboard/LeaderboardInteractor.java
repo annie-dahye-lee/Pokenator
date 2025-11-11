60aa509a932b90e3a724f224ceac9d547ade3592
Sunny Lin <iluvsunshinezduqz@gmail.com>
2025-11-10 19:03:13 -0500

    Worked more on the leaderboard use case. Need to figure out how the DAO will get all user objects.

--- src/main/java/use_case/leaderboard/LeaderboardInteractor.java
+++ src/main/java/use_case/leaderboard/LeaderboardInteractor.java
@@ -1,4 +1,18 @@
 package use_case.leaderboard;
 
-public class LeaderboardInteractor {
+public class LeaderboardInteractor implements LeaderboardInputBoundary {
+
+    private final ProfilesDataAccessInterface profilesDAO;
+    private final LeaderboardOutputBoundary leaderboardPresenter;
+
+    public LeaderboardInteractor(ProfilesDataAccessInterface profilesDAO, LeaderboardOutputBoundary leaderboardPresenter) {
+        this.profilesDAO = profilesDAO;
+        this.leaderboardPresenter = leaderboardPresenter;
+    }
+
+    @Override
+    public void execute(LeaderboardInputData leaderboardInputData) {
+
+    }
+
 }
