8e91de5b5da4a3f0706dad4b9ed516f6a83bd7ab
ykatyyang <katy.yang@mail.utoronto.ca>
2025-12-02 17:47:44 -0500

    Update ChooseFavPokemonOutputData.java

--- src/main/java/use_case/choose_fav_pokemon/ChooseFavPokemonOutputData.java
+++ src/main/java/use_case/choose_fav_pokemon/ChooseFavPokemonOutputData.java
@@ -5,21 +5,11 @@ package use_case.choose_fav_pokemon;
  */
 public class ChooseFavPokemonOutputData {
 
-    private final String username;
-    private final String bio;
     private final String favPokemon;
 
-    public ChooseFavPokemonOutputData(String username, String bio, String favPokemon) {
-        this.username = username;
-        this.bio = bio;
+    public ChooseFavPokemonOutputData(String favPokemon) {
         this.favPokemon = favPokemon;
     }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public String getBio() { return bio; }
 
     public String getFavPokemon() { return favPokemon; }
 }
